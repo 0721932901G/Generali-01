99566d934cd0a5c5485652c66696c4887ea76279
0721932901G <160822951+0721932901G@users.noreply.github.com>
2025-03-12 07:49:51 +0300

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Generali,
+  author: 'Generali,
+  botName: "Generali",
+  botOwner: 'Generali', // Your name
+  ownerNumber: '254721932901', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
